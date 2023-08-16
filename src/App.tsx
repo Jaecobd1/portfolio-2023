edd599d0cbdab51240485a36a297f849424dee27
Jake Dobler <jaecob.dobler@reusser.com>
2023-08-15 23:40:36 -0400

    fixed analytics

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,20 @@ import Portfolio from './components/Portfolio'
 import Summary from './components/Summary'
 import About from './components/About'
 import { Toaster } from 'react-hot-toast'
+import { Analytics } from '@vercel/analytics/react'
 function App() {
   return (
-    <MantineProvider withNormalizeCSS>
-      <Nav />
-      <Hero />
-      <Portfolio />
-      <Summary />
-      <About />
-      <Toaster />
-    </MantineProvider>
+    <>
+      <MantineProvider withNormalizeCSS>
+        <Nav />
+        <Hero />
+        <Portfolio />
+        <Summary />
+        <About />
+        <Toaster />
+      </MantineProvider>
+      <Analytics />
+    </>
   )
 }
 
